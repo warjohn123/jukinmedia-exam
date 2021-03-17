ee90ae42ca5feadeb44b17b16fa9d95779be0e37
warjohn123 <warrencaruana1@gmail.com>
2021-03-17 16:07:16 +0800

    Modify date

--- src/app/pages/videos-list/components/video-filter-bar.component.tsx
+++ src/app/pages/videos-list/components/video-filter-bar.component.tsx
@@ -1,37 +1,69 @@
-import React from "react";
-import { VideoItemComponent } from "./video-item.component";
+import React, { useEffect, useState } from "react";
 
 import styles from "../videos-list.module.scss";
 import { Col, Form, Row } from "react-bootstrap";
 
+interface DateDisplay {
+    hours: string;
+    minutes: string;
+    seconds: string;
+    ampm: string;
+}
 
 export function VideoFilterBarComponent(props: any) {
+
+    const formatAMPM = (date: Date): DateDisplay => {
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var seconds = date.getSeconds();
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        let hoursStr = hours ? hours.toString() : '12'; // the hour '0' should be '12'
+        let minutesStr = minutes < 10 ? '0' + minutes : minutes.toString();
+        let secondsStr = seconds < 10 ? '0' + seconds : seconds.toString();
+        return {
+            hours: hoursStr,
+            minutes: minutesStr,
+            seconds: secondsStr,
+            ampm: ampm
+        };
+    }
+    const [date, setDate] = useState<DateDisplay>(formatAMPM(new Date()));
+
+    const updateDate = () => {
+        setDate(formatAMPM(new Date()));
+    };
+
+    useEffect(() => {
+        setInterval(updateDate, 1000);
+    })
 
     return (
         <div className={styles.VideoFilterBarContainer}>
             <Row>
                 <Col sm={12} md={8}>
-
                     <Row>
                         <Col lg={4} md={6} sm={6}>
-                            <ul className={styles.VideoFilterClock}>
-                                <li>02</li>&nbsp;
-                            <li>:</li>&nbsp;
-                            <li>47</li>&nbsp;
-                            <li>:</li>&nbsp;
-                            <li>13</li>&nbsp;
-                            <li>PM</li>&nbsp;
-                        </ul>
+                            <div className={styles.VideoFilterClock}>
+                                <li>{date?.hours}</li>&nbsp;
+                                <li>:</li>&nbsp;
+                                <li>{date?.minutes}</li>&nbsp;
+                                <li>:</li>&nbsp;
+                                <li>{date?.seconds}</li>&nbsp;
+                                <li>{date?.ampm}</li>&nbsp;
+                            </div>
                         </Col>
 
                         <Col lg={8} md={6} sm={6}>
                             <div className={styles.VideoFilterDate + " d-none d-sm-block"}>
                                 <span className={styles.VideoFilterClockDay}>Wednesday</span>
-                                <span>17 March 2021</span>
+                                <span>
+                                    {new Date().toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' })}
+                                </span>
                             </div>
-                            <div className="d-block d-sm-none">
-                                Wednesday 17 March 2021
-                        </div>
+                            <div className="d-block d-sm-none text-center mb-5">
+                                {new Date().toLocaleDateString("en-US", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
+                            </div>
 
                         </Col>
                     </Row>
