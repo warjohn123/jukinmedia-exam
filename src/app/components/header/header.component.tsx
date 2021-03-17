c154d8184cf1ab11b6102b6c156f564f47433597
warjohn123 <warrencaruana1@gmail.com>
2021-03-17 16:11:28 +0800

    Modify header

--- src/app/components/header/header.component.tsx
+++ src/app/components/header/header.component.tsx
@@ -12,12 +12,12 @@ export function HeaderComponent() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link style={{ color: 'white' }}>Categories</Nav.Link>
-                        <Nav.Link style={{ color: 'white' }}>Collections</Nav.Link>
-                        <Nav.Link style={{ color: 'white' }}>FAQ</Nav.Link>
-                        <Nav.Link style={{ color: 'white' }}>Pricing</Nav.Link>
-                        <Nav.Link style={{ color: 'white' }}>The Wire</Nav.Link>
-                        <Nav.Link style={{ color: 'white' }}>Contact Us</Nav.Link>
+                        <Nav.Link className={styles.NavLink}>Categories</Nav.Link>
+                        <Nav.Link className={styles.NavLink}>Collections</Nav.Link>
+                        <Nav.Link className={styles.NavLink}>FAQ</Nav.Link>
+                        <Nav.Link className={styles.NavLink}>Pricing</Nav.Link>
+                        <Nav.Link className={styles.NavLink}>The Wire</Nav.Link>
+                        <Nav.Link className={styles.NavLink}>Contact Us</Nav.Link>
                         <Button variant="outline-light" style={{ marginRight: 7 }}>SIGN IN</Button>
                         <Button variant="danger" style={{ marginLeft: 7 }}>SIGN UP</Button>
                     </Nav>
